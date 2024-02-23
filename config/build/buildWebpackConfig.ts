9b96df7b15f04e4c6f5ba83536c707b869a65f79
rpellya@gmail.com <you@example.com>
2024-02-23 16:23:05 +0300

    Initial project

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,28 @@
+import path from 'path'
+import webpack from 'webpack'
+import { buildLoaders } from './buildLoaders'
+import { buildPlugins } from './buildPlugins'
+import { buildResolvers } from './buildResolvers'
+import { BuildOptions } from './types/config'
+import { buildDevServer } from './buildDevServer'
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+	const { mode, paths, isDev } = options
+
+	return {
+		entry: paths.entry,
+		mode: mode,
+		module: {
+			rules: buildLoaders(options),
+		},
+		output: {
+			filename: '[name].[contenthash].js',
+			path: paths.build,
+			clean: true,
+		},
+		plugins: buildPlugins(options),
+		resolve: buildResolvers(),
+		devtool: isDev ? 'inline-source-map' : undefined,
+		devServer: isDev ? buildDevServer(options) : undefined,
+	}
+}
---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { Suspense } from 'react'
+import { AboutPageAsync } from './pages/About/AboutPage.async'
+import { MainPageAsync } from './pages/MainPage/MainPage.async'
+import '../index.scss'
+
+export function App() {
+	return (
+		<div className='App'>
+			<Link to={'/'}>Main</Link>
+			<Link to={'/about'}>About the site</Link>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+					<Route path={'/'} element={<MainPageAsync />} />
+					<Route path={'/about'} element={<AboutPageAsync />} />
+				</Routes>
+			</Suspense>
+		</div>
+	)
+}
