96e4f8a53cb053cc469c565f028a793f8721fe02
Пелля Роман Юрьевич <pellya-r@da.lan>
2024-03-01 17:52:14 +0300

    Add routeConfig and move routes in AppRouter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,7 @@
-import { Suspense } from 'react'
-import { Route, Routes } from 'react-router-dom'
 import { Link } from 'react-router-dom'
-import { AboutPageAsync } from 'pages/AboutPage/ui/AboutPage.async'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { useTheme } from './providers/ThemeProvider'
-import { AboutPage } from 'pages/AboutPage'
-import { MainPage } from 'pages/MainPage'
+import { AppRouter } from './providers/router'
 import './styles/index.scss'
 
 const App = () => {
@@ -16,12 +12,7 @@ const App = () => {
 			<Link to={'/'}>Main</Link>
 			<Link to={'/about'}>About the site</Link>
 			<button onClick={toggleTheme}>toggleTheme</button>
-			<Suspense fallback={<div>Loading...</div>}>
-				<Routes>
-					<Route path={'/'} element={<MainPage />} />
-					<Route path={'/about'} element={<AboutPage />} />
-				</Routes>
-			</Suspense>
+			<AppRouter />
 		</div>
 	)
 }
