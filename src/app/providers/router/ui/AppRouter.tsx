392bdac41f5053c67aa70b6905dbda148e42ad6e
rpellya@gmail.com <you@example.com>
2024-03-01 18:05:38 +0300

    Some fix

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,23 +1,17 @@
-import React, { Suspense } from 'react'
-import { MainPage } from 'pages/MainPage'
+import { Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { AboutPage } from 'pages/AboutPage'
 import { routeConfig } from 'shared/config/routeConfig/routeConfig'
 
 const AppRouter = () => {
-    return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-                {Object.values(routeConfig).map(({ element, path }) => (
-                    <Route
-                        key={path}
-                        path={path}
-                        element={element}
-                    />
-                ))}
-            </Routes>
-        </Suspense>
-    )
+	return (
+		<Suspense fallback={<div>Loading...</div>}>
+			<Routes>
+				{Object.values(routeConfig).map(({ element, path }) => (
+					<Route key={path} path={path} element={element} />
+				))}
+			</Routes>
+		</Suspense>
+	)
 }
 
 export default AppRouter
