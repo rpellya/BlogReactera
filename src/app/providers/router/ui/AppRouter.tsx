694671936dc0e4ee86e95f9058bf7f9ab315fbff
Пелля Роман Юрьевич <pellya-r@da.lan>
2024-03-04 10:59:19 +0300

    Fix the code for routes

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,7 +1,5 @@
-import React, { Suspense } from 'react'
-import { MainPage } from 'pages/MainPage'
+import { Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { AboutPage } from 'pages/AboutPage'
 import { routeConfig } from 'shared/config/routeConfig/routeConfig'
 
 const AppRouter = () => {
