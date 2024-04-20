48344074aead726ed64561e63978759dbca6ba3f
Pellya <rpellya@gmail.com>
2024-04-20 21:20:33 +0300

    Fix some code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,11 @@
-import { Link } from 'react-router-dom'
-import { classNames } from 'shared/lib/classNames/classNames'
-import { useTheme } from './providers/ThemeProvider'
-import { AppRouter } from './providers/router'
-import './styles/index.scss'
+import { Link } from 'react-router-dom';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from './providers/ThemeProvider';
+import { AppRouter } from './providers/router';
+import './styles/index.scss';
 
 const App = () => {
-	const { theme, toggleTheme } = useTheme()
-
+	const { theme, toggleTheme } = useTheme();
 	return (
 		<div className={classNames('App', {}, [theme])}>
 			<Link to={'/'}>Main</Link>
@@ -14,7 +13,7 @@ const App = () => {
 			<button onClick={toggleTheme}>toggleTheme</button>
 			<AppRouter />
 		</div>
-	)
-}
+	);
+};
 
-export default App
+export default App;
