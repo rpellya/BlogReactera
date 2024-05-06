5858a09d1bdc551342c754d8b5a71d8dc18cf664
Pellya <rpellya@gmail.com>
2024-05-06 23:09:55 +0300

    Add babel and extract plugib

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -8,6 +8,26 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 		use: ['@svgr/webpack'],
 	};
 
+	const babelLoader = {
+		test: /\.(js|jsx|tsx)$/,
+		exclude: /node_modules/,
+		use: {
+			loader: "babel-loader",
+			options: {
+				presets: ['@babel/preset-env'],
+				"plugins": [
+					[
+						"i18next-extract",
+						{
+							locales: ['ru', 'en'],
+							keyAsDefaultValue: true
+						}
+					]
+				]
+			}
+		}
+	}
+
 	const cssLoader = {
 		test: /\.s[ac]ss$/i,
 		use: [
@@ -47,5 +67,12 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 		],
 	};
 
-	return [typescirptLoader, cssLoader, fontLoader, svgLoader, fileLoader];
+	return [
+		fileLoader,
+		svgLoader,
+		babelLoader,
+		typescirptLoader,
+		cssLoader,
+		fontLoader,
+	];
 }
