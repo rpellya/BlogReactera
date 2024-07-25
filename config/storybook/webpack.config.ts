7ed6d34f8dd5c9a55554c89690c275634d693701
Pellya <rpellya@gmail.com>
2024-07-25 16:27:05 +0300

    Add exlude for tsconfig

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,4 +1,3 @@
-/* eslint-disable-line */
 import webpack, { RuleSetRule } from 'webpack';
 import path from 'path';
 import { buildCssLoader } from '../build/loaders/buildCssLoader';
@@ -15,7 +14,6 @@ export default ({ config }: { config: webpack.Configuration }) => {
         config.resolve?.modules?.push(paths.src);
         config.resolve?.extensions?.push('.ts', '.tsx');
 
-        // eslint-disable-next-line no-param-reassign
         config.module.rules = config.module?.rules?.map((rule: RuleSetRule) => {
             if (/svg/.test(rule.test as string)) {
                 return { ...rule, exclude: /\.svg$/i };
