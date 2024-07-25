a3b229961318988de9cbbbc7e7aef29404562660
Pellya <rpellya@gmail.com>
2024-07-25 17:08:46 +0300

    fix import for cssloader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,7 @@
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
-import { buildCssLoader } from './loaders/buildCssLoader';
+
+const { buildCssLoader } = require('./loaders/buildCssLoader.cjs');
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     const svgLoader = {
