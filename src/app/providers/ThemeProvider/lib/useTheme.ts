ae327046978a8321273efee3b8e507a2032db6e0
Pellya <rpellya@gmail.com>
2024-07-31 10:25:21 +0300

    Update array of depends

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -13,7 +13,7 @@ export function useTheme(): useThemeResult {
         const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
         setTheme(newTheme);
         localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
-    }, [theme]);
+    }, [setTheme, theme]);
 
     return { theme, toggleTheme };
 }
