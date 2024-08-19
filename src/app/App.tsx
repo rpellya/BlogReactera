ac340f6c43103dad89c93291e6b140b401aea9ef
Pellya <rpellya@gmail.com>
2024-08-19 15:28:43 +0300

    Fix the code and theme

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,16 @@
-import { Suspense, useState } from 'react';
+import { Suspense } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { Modal } from 'shared/ui/Modal/Modal';
-import { Button } from 'shared/ui/Button/Button';
 import { AppRouter } from './providers/router';
 import { useTheme } from './providers/ThemeProvider';
 
 const App = () => {
     const { theme } = useTheme();
+    document.body.className = theme;
 
     return (
-        <div className={classNames('App', {}, [theme])}>
+        <div className={classNames('App', {}, [])}>
             <Suspense fallback="">
                 <Navbar />
                 <div className="content-page">
