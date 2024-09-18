91b3de393f9e23ea44db46312222a34f53cdc87a
Pellya <rpellya@gmail.com>
2024-09-18 15:02:36 +0300

    add some comments

--- src/pages/AboutPage/ui/AboutPage.async.tsx
+++ src/pages/AboutPage/ui/AboutPage.async.tsx
@@ -1,3 +1,8 @@
 import { lazy } from 'react';
 
 export const AboutPageAsync = lazy(() => import('./AboutPage'));
+
+// Когда можно использовать
+// 1. Страницы
+// 2. Модалки/шторки/тултипы
+// 3. Блоки, которые не попадают в экраны
