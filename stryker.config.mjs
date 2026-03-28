34075973ac82b4012fb5d18f0631ea2937d59985
Pellya Roman <pellya-r@datagile.ru>
2026-03-28 12:58:02 +0300

    Add mutation tests for MTPO

---
+++ stryker.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+/** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
+const config = {
+    _comment:
+        // eslint-disable-next-line max-len
+        "This config was generated using 'stryker init'. Please take a look at: https://stryker-mutator.io/docs/stryker-js/configuration/ for more information.",
+    packageManager: 'npm',
+    reporters: ['html', 'clear-text', 'progress'],
+    testRunner: 'jest',
+
+    testRunner_comment:
+        'Take a look at https://stryker-mutator.io/docs/stryker-js/jest-runner for information about the jest plugin.',
+    coverageAnalysis: 'perTest',
+};
+export default config;
