a44adfd4dfe7c5f85252a52474bc954394acf601
Pellya Roman <pellya-r@datagile.ru>
2026-06-24 23:47:04 +0300

    Add new components for wrapping components content

--- src/entities/Article/ui/AticleDetails/ArticleDetails.tsx
+++ src/entities/Article/ui/AticleDetails/ArticleDetails.tsx
@@ -14,6 +14,7 @@ import EyeIcon from 'shared/assets/icons/eye.svg';
 import CalendarIcon from 'shared/assets/icons/calendar.svg';
 import { Icon } from 'shared/ui/Icon/Icon';
 import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
+import { HStack } from 'shared/ui/Stack';
 import { ArticleBlock, ArticleBlockType } from '../../model/types/article';
 import { fetchArticleById } from '../../model/services/fetchArticleById/fetchArticleById';
 import cls from './ArticleDetails.module.scss';
@@ -35,7 +36,7 @@ interface ArticleDetailsProps {
 const reducers: ReducersList = {
     articleDetails: articleDetailsReducer,
 };
-/* eslint-disable indent */
+
 const renderBlock = (block: ArticleBlock) => {
     switch (block.type) {
         case ArticleBlockType.CODE:
@@ -66,7 +67,6 @@ const renderBlock = (block: ArticleBlock) => {
             return null;
     }
 };
-/* eslint-enable indent */
 
 export const ArticleDetails = memo(({ className, id }: ArticleDetailsProps) => {
     const { t } = useTranslation('article-details');
@@ -107,13 +107,13 @@ export const ArticleDetails = memo(({ className, id }: ArticleDetailsProps) => {
     } else {
         content = (
             <>
-                <div className={cls.avatarWrapper}>
+                <HStack justify="center" max>
                     <Avatar
                         className={cls.avatar}
                         size={200}
                         src={article?.img}
                     />
-                </div>
+                </HStack>
                 <Text
                     text={article?.subtitle}
                     title={article?.title}
