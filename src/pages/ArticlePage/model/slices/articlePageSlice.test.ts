f4c06d14af8991142cc24c51573b14346f91bcfb
Pellya Roman <pellya-r@datagile.ru>
2026-06-30 13:02:58 +0300

    Add new test for articlesPageReducer

--- src/pages/ArticlePage/model/slices/articlePageSlice.test.ts
+++ src/pages/ArticlePage/model/slices/articlePageSlice.test.ts
@@ -1,6 +1,7 @@
-import { ArticleView } from 'entities/Article';
+import { Article, ArticleView } from 'entities/Article';
 import { ArticlesPageSchema } from '../types/articlesPageSchema';
 import { articlesPageAction, articlesPageReducer } from './articlePageSlice';
+import { fetchArticlesList } from '../services/fetchArticlesList/fetchArticlesList';
 
 describe('articlePageSlice.test', () => {
     test('Test set view', () => {
@@ -13,5 +14,24 @@ describe('articlePageSlice.test', () => {
                 articlesPageAction.setView(ArticleView.LIST),
             ),
         ).toEqual({ view: ArticleView.LIST });
+    });
+
+    test('Test update profile service fulfilled', () => {
+        const state: DeepPartial<ArticlesPageSchema> = {
+            isLoading: true,
+        };
+        expect(
+            articlesPageReducer(
+                state as ArticlesPageSchema,
+                fetchArticlesList.fulfilled([{ id: '1' } as Article], '', {
+                    replace: true,
+                }),
+            ),
+        ).toEqual({
+            entities: { 1: { id: '1' } },
+            hasMore: false,
+            isLoading: false,
+            ids: ['1'],
+        });
     });
 });
